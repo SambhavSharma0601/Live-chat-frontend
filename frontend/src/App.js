a358e16476ce2a43a70bbea333a6402802b6bde7
SambhavSharma0601 <145418972+SambhavSharma0601@users.noreply.github.com>
2025-02-16 18:08:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,6 @@ import "./App.css";
 import ChatUI from "./components/ChatUI";
 
 function App() {
-  const [response, setResponse] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/hello") 
-      .then((res) => res.json())
-      .then((data) => setResponse(data.message)) 
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-  console.log(response)
 
   return (
     <div className="App1">
